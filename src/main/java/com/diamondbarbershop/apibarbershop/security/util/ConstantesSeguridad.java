0ade8803ad8a183c1f7c533dab84d41688295bf6
Arian Sevillano <ariansevillano1@gmail.com>
2026-05-15 06:16:18 -0500

    feat(security): upgrade de librerías para jwt, upgrade de mecanismos de validacion de token, uso de oauth2

--- src/main/java/com/diamondbarbershop/apibarbershop/security/util/ConstantesSeguridad.java
+++ src/main/java/com/diamondbarbershop/apibarbershop/security/util/ConstantesSeguridad.java
@@ -2,8 +2,7 @@ package com.diamondbarbershop.apibarbershop.security.util;
 
 public class ConstantesSeguridad {
     public static final long JWT_EXPIRATION_TOKEN = 300000; //equivale a 5 min, donde 60000 = a 1 min
-    public static final String JWT_FIRMA = "firma";
-
+    //harcodeado, solo para pruebas
+    public static final String JWT_FIRMA = "DiamondBarberHub-Secret-Key-For-JWT-Signing-HS512-Algorithm-Needs-64-Bytes-Min!!";
     public static final long JWT_REFRESH_EXPIRATION = 604800000; // 7 días
-
 }
